ba2f92cd452f60744601ff3b5cf6f3bfee194eba
Fmoyinzunza <Mmoises.inzg@gmail.com>
2022-11-02 02:23:34 +0000

    changed to aws

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'America/New York',
+    'timezone' => 'America/New_York',
 
     /*
     |--------------------------------------------------------------------------
